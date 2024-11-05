dd285e8d5587292a693c6114cc2a3c1caeb0fc74
gem install rails <gem install rails>
2024-11-05 02:46:12 -0400

    Fin del Feature

--- src/App.js
+++ src/App.js
@@ -9,6 +9,24 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+
+
+
+
+
+
+        <h1>Feature1</h1>
+
+
+
+
+
+
+
+
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
