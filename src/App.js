34b88c668bea1905d8cdf73959fc5cb1b8574f41
gem install rails <gem install rails>
2024-11-14 09:38:36 -0400

    Proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import backgroundImage from './Pagina.png'; // Importa la imagen como backgroundImage
 
 function App() {
   return (
-    <div className="container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+
+
+
+
+
+
+        <h1>Feature1</h1>
+        <h1>Release V1.0.2</h1>
+        <h1>Hotfix v1.0.3</h1>
+
+
+
+
+
+
+
+
+
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
