432a2fdbf63e087d7c1e1a289e47e22be97285cf
gem install rails <gem install rails>
2024-11-14 09:31:17 -0400

    Proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import backgroundImage from './Pagina.png'; // Importa la imagen como backgroundImage
 
 function App() {
   return (
-    <div className="container">
+    <div className="container" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="left-section"></div>
       <div className="right-section">
         <div className="horizontal-line"></div>
