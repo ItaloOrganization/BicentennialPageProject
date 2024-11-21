807a5d415c6f54e38aef4eeee740ac61720c67b1
gem install rails <gem install rails>
2024-11-21 09:45:34 -0400

    PaginaEnProcesoDerechaAbajo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -6,10 +6,19 @@ function App() {
     <div className="container">
       <div className="left-section"></div>
       <div className="right-section">
-        <div className="horizontal-line"></div>
-        <div className="circle"></div>
+        <div className="top-section">
+          <div className="horizontal-line"></div>
+          <div className="circle"></div>
+          <div className="circlee"></div>
+        </div>
+        <div className="bottom-section">
+          <div className="lorem-text">
+            Desde Sucre, ciudad blanca de los 4 nombres, cuna de la libertad latinoamericana, la<br />
+            CAINCO Chuquisaca, te rinde homenaje de emprendimiento, empleo y oportunidad.
+          </div>
+        </div>
       </div>
-    </div>
+    </div>  
   );
 }
 
