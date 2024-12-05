9c6dbf6936b073d874def89af912fa3bbc1413c4
gem install rails <gem install rails>
2024-12-05 00:29:16 -0400

    ProyectoConNuevaVentana

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import OtherPage from './OtherPage';
 import './App.css';
 
-function App() {
+function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="container">
       <div className="left-section"></div>
@@ -12,64 +16,88 @@ function App() {
           <div className="circlee"></div>
         </div>
         <div className="bottom-section">
-          <div   className="lorem-text"
-  style={{
-    fontSize: "23px",
-    color: "white"
-  }}>
+          <div
+            className="lorem-text"
+            style={{
+              fontSize: '23px',
+              color: 'white',
+            }}
+          >
             Desde Sucre, ciudad blanca de los 4 nombres, cuna de la libertad latinoamericana, la<br />
             CAINCO Chuquisaca, te rinde homenaje de emprendimiento, empleo y oportunidad.
           </div>
-          <div className="Contenedor-Rectangulos"> 
-          <div class="rectangulo">
-            <div class="inner"></div>
-            <div class="border-only">
-              <div class="top">
-                <div class="left"></div>
-                <div class="right">
-                  <div class="top-half"></div>
-                  <div class="bottom-half"></div>
+          <div className="Contenedor-Rectangulos">
+            <div
+              className="rectangulo"
+              onClick={() => navigate('/other-page')}
+              style={{ cursor: 'pointer' }}
+            >
+              <div className="inner"></div>
+              <div className="border-only">
+                <div className="top">
+                  <div className="left"></div>
+                  <div className="right">
+                    <div className="top-half"></div>
+                    <div className="bottom-half"></div>
+                  </div>
                 </div>
+                <div className="bottom"></div>
               </div>
-              <div class="bottom"></div>
+              <div className="productos">NUESTROS PRODUCTOS</div>
             </div>
-            <div class="productos">NUESTROS PRODUCTOS</div>
-          </div>
 
-          <div class="rectangulo">
-            <div class="inner"></div>
-            <div class="border-only">
-              <div class="top">
-                <div class="left"></div>
-                <div class="right">
-                  <div class="top-half"></div>
-                  <div class="bottom-half"></div>
+            <div
+              className="rectangulo"
+              onClick={() => navigate('/other-page')}
+              style={{ cursor: 'pointer' }}
+            >
+              <div className="inner"></div>
+              <div className="border-only">
+                <div className="top">
+                  <div className="left"></div>
+                  <div className="right">
+                    <div className="top-half"></div>
+                    <div className="bottom-half"></div>
+                  </div>
                 </div>
+                <div className="bottom"></div>
               </div>
-              <div class="bottom"></div>
+              <div className="productos">NUESTROS PRODUCTOS</div>
             </div>
-            <div class="productos">NUESTROS PRODUCTOS</div>
-          </div>
 
-          <div class="rectangulo">
-            <div class="inner"></div>
-            <div class="border-only">
-              <div class="top">
-                <div class="left"></div>
-                <div class="right">
-                  <div class="top-half"></div>
-                  <div class="bottom-half"></div>
+            <div
+              className="rectangulo"
+              onClick={() => navigate('/other-page')}
+              style={{ cursor: 'pointer' }}
+            >
+              <div className="inner"></div>
+              <div className="border-only">
+                <div className="top">
+                  <div className="left"></div>
+                  <div className="right">
+                    <div className="top-half"></div>
+                    <div className="bottom-half"></div>
+                  </div>
                 </div>
+                <div className="bottom"></div>
               </div>
-              <div class="bottom"></div>
+              <div className="productos">NUESTROS PRODUCTOS</div>
             </div>
-            <div class="productos">NUESTROS PRODUCTOS</div>
-          </div>
           </div>
         </div>
       </div>
     </div>
+  );
+}
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/other-page" element={<OtherPage />} />
+      </Routes>
+    </Router>
   );
 }
 
